e276c6e2332ff430d61bf1bcb21bac59822301b6
Marius Runde <marius.runde@uni-muenster.de>
2016-11-11 12:41:07 +0100

    Fixed FORECAST_WEATHER_URL in weather constants

--- app/src/components/weather/constants.js
+++ app/src/components/weather/constants.js
@@ -10,7 +10,7 @@ const API_KEY = 'YOUR_API_KEY';
 
 // Customized OpenWeatherMap URLs to request current and forecast weather data
 const CURRENT_WEATHER_URL = 'http://api.openweathermap.org/data/2.5/weather?APPID=' + API_KEY + '&q=';
-const FORECAST_WEATHER_URL = 'api.openweathermap.org/data/2.5/forecast?APPID=' + API_KEY + '&q=';
+const FORECAST_WEATHER_URL = 'http://api.openweathermap.org/data/2.5/forecast?APPID=' + API_KEY + '&q=';
 
 // General URL for the OpenWeatherMap icons
 const WEATHER_ICON_URL = 'http://openweathermap.org/img/w/';
